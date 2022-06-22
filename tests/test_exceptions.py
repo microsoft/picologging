83cce4e668021961f0f0326d0a38beeafcea444c
Anthony Shaw <anthony.p.shaw@gmail.com>
2022-06-22 16:21:10 +1000

    Modify the exception capture test to use a custom handler instead of the fallback handler

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,15 +1,18 @@
 import picologging
+import io
 
 picologging.basicConfig()
 
 
 def test_picologging_handler(capsys):
     logger = picologging.getLogger(__name__)
+    tmp = io.StringIO()
+    handler = picologging.StreamHandler(tmp)
+    logger.addHandler(handler)
     try:
         1/0
     except ZeroDivisionError:
         logger.exception("bork")
-    cap = capsys.readouterr()
-    result = cap.err + cap.out
+    result = tmp.getvalue()
     assert "bork" in result
     assert "ZeroDivisionError: division by zero" in result
