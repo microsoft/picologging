121199dae7b550d5d17603bc60c122e2e48c0f84
Anthony Shaw <anthony.p.shaw@gmail.com>
2022-06-28 15:14:23 +1000

    Add profile script

---
+++ tests/profile/profile.py
@@ -0,0 +1,20 @@
+import picologging
+from io import StringIO
+
+
+def run_profile(level = picologging.DEBUG):
+    logger = picologging.Logger("test", level)
+    tmp = StringIO()
+
+    handler = picologging.StreamHandler(tmp)
+    handler.setLevel(level)
+    formatter = picologging.Formatter('%(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.handlers.append(handler)
+
+    for _ in range(100_000):
+        logger.debug("There has been a picologging issue %s %s %s", 1, 2, 3)
+    
+    assert tmp.getvalue() != ""
+
+run_profile()
