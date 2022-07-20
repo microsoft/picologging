190c2ef0d8a8c93618cfa881cf3a1c19423248f2
Amin Alaee <mohammadamin.alaee@gmail.com>
2022-07-20 20:00:50 +0200

    Add SocketHandler

--- src/picologging/handlers.py
+++ src/picologging/handlers.py
@@ -1,6 +1,9 @@
 import queue
 import os
+import pickle
 import re
+import socket
+import struct
 import time
 import threading
 import picologging
@@ -765,3 +768,163 @@ class MemoryHandler(BufferingHandler):
         self.buffer.append(record)
         if (len(self.buffer) >= self.capacity) or (record.levelno >= self.flushLevel):
             self.flush()
+
+
+class SocketHandler(picologging.Handler):
+    """
+    A handler class which writes logging records, in pickle format, to
+    a streaming socket. The socket is kept open across logging calls.
+    If the peer resets it, an attempt is made to reconnect on the next call.
+    The pickle which is sent is that of the LogRecord's attribute dictionary
+    (__dict__), so that the receiver does not need to have the logging module
+    installed in order to process the logging event.
+    To unpickle the record at the receiving end into a LogRecord, use the
+    makeLogRecord function.
+    """
+
+    def __init__(self, host, port):
+        """
+        Initializes the handler with a specific host address and port.
+        When the attribute *closeOnError* is set to True - if a socket error
+        occurs, the socket is silently closed and then reopened on the next
+        logging call.
+        """
+        picologging.Handler.__init__(self)
+        self.host = host
+        self.port = port
+        if port is None:
+            self.address = host
+        else:
+            self.address = (host, port)
+        self.sock = None
+        self.closeOnError = False
+        self.retryTime = None
+        # Exponential backoff parameters.
+        self.retryStart = 1.0
+        self.retryMax = 30.0
+        self.retryFactor = 2.0
+
+    def makeSocket(self, timeout=1):
+        """
+        A factory method which allows subclasses to define the precise
+        type of socket they want.
+        """
+        if self.port is not None:
+            result = socket.create_connection(self.address, timeout=timeout)
+        else:
+            result = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+            result.settimeout(timeout)
+            try:
+                result.connect(self.address)
+            except OSError:
+                result.close()  # Issue 19182
+                raise
+        return result
+
+    def createSocket(self):
+        """
+        Try to create a socket, using an exponential backoff with
+        a max retry time.
+        """
+        now = time.time()
+        # Either retryTime is None, in which case this
+        # is the first time back after a disconnect, or
+        # we've waited long enough.
+        if self.retryTime is None:
+            attempt = True
+        else:
+            attempt = now >= self.retryTime
+        if attempt:
+            try:
+                self.sock = self.makeSocket()
+                self.retryTime = None  # next time, no delay before trying
+            except OSError:
+                # Creation failed, so set the retry time and return.
+                if self.retryTime is None:
+                    self.retryPeriod = self.retryStart
+                else:
+                    self.retryPeriod = self.retryPeriod * self.retryFactor
+                    if self.retryPeriod > self.retryMax:
+                        self.retryPeriod = self.retryMax
+                self.retryTime = now + self.retryPeriod
+
+    def send(self, s):
+        """
+        Send a pickled string to the socket.
+        This function allows for partial sends which can happen when the
+        network is busy.
+        """
+        if self.sock is None:
+            self.createSocket()
+        # self.sock can be None either because we haven't reached the retry
+        # time yet, or because we have reached the retry time and retried,
+        # but are still unable to connect.
+        if self.sock:
+            try:
+                self.sock.sendall(s)
+            except OSError:  # pragma: no cover
+                self.sock.close()
+                self.sock = None  # so we can call createSocket next time
+
+    def makePickle(self, record):
+        """
+        Pickles the record in binary format with a length prefix, and
+        returns it ready for transmission across the socket.
+        """
+        ei = record.exc_info
+        if ei:
+            # just to get traceback text into record.exc_text ...
+            _ = self.format(record)
+        # See issue #14436: If msg or args are objects, they may not be
+        # available on the receiving end. So we convert the msg % args
+        # to a string, save it as msg and zap the args.
+        d = dict(record.__dict__)
+        d["msg"] = record.getMessage()
+        d["args"] = None
+        d["exc_info"] = None
+        # Issue #25685: delete 'message' if present: redundant with 'msg'
+        d.pop("message", None)
+        s = pickle.dumps(d, 1)
+        slen = struct.pack(">L", len(s))
+        return slen + s
+
+    def handleError(self, record):
+        """
+        Handle an error during logging.
+        An error has occurred during logging. Most likely cause -
+        connection lost. Close the socket so that we can retry on the
+        next event.
+        """
+        if self.closeOnError and self.sock:
+            self.sock.close()
+            self.sock = None  # try to reconnect next time
+        else:
+            picologging.Handler.handleError(self, record)
+
+    def emit(self, record):
+        """
+        Emit a record.
+        Pickles the record and writes it to the socket in binary format.
+        If there is an error with the socket, silently drop the packet.
+        If there was a problem with the socket, re-establishes the
+        socket.
+        """
+        try:
+            s = self.makePickle(record)
+            self.send(s)
+        except Exception:
+            self.handleError(record)
+
+    def close(self):
+        """
+        Closes the socket.
+        """
+        self.acquire()
+        try:
+            sock = self.sock
+            if sock:
+                self.sock = None
+                sock.close()
+            picologging.Handler.close(self)
+        finally:
+            self.release()
