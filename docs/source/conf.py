c3528e283373e13c2bda98127bea127e52202e15
Anthony Shaw <anthony.p.shaw@gmail.com>
2022-08-08 12:48:22 +1000

    reformat the config file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'picologging'
-copyright = '2022, Microsoft'
-author = 'Microsoft'
+project = "picologging"
+copyright = "2022, Microsoft"
+author = "Microsoft"
 
 
 # -- General configuration ---------------------------------------------------
@@ -27,10 +27,10 @@ author = 'Microsoft'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -43,9 +43,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'furo'
+html_theme = "furo"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
