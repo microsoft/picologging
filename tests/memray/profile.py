0f71f117fffe3f7a0f4d7af6c42daad73570ca0e
Anthony Shaw <anthony.p.shaw@gmail.com>
2022-08-10 06:43:23 +0000

    Profile all the fields

--- tests/memray/profile.py
+++ tests/memray/profile.py
@@ -8,7 +8,7 @@ def log(level=picologging.INFO):
 
     handler = picologging.StreamHandler(tmp)
     handler.setLevel(level)
-    formatter = picologging.Formatter("%(created)f %(pathname)s:%(module)s:%(filename)s:%(lineno)d %(funcName)s %(levelno)d %(name)s - %(levelname)s %(process)d %(thread)d- %(message)s")
+    formatter = picologging.Formatter("%(created)f/%(asctime)s %(pathname)s:%(module)s:%(filename)s:%(lineno)d %(funcName)s %(levelno)d %(name)s - %(levelname)s %(process)d %(thread)d- %(message)s")
     handler.setFormatter(formatter)
     logger.handlers.append(handler)
 
