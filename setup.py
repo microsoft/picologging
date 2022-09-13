ffccd8bc90facb9cc8ba45f2ba283548ccaf54a8
Amin Alaee <mohammadamin.alaee@gmail.com>
2022-09-13 17:33:48 +0200

    Update project classifiers

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Topic :: System :: Logging",
         "Programming Language :: Python :: Implementation :: CPython",
