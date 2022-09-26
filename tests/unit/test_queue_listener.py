564643ce878f5ecc94cd99af8d4d31c7394871d6
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2022-09-26 19:15:32 +0200

    added passing test

--- tests/unit/test_queue_listener.py
+++ tests/unit/test_queue_listener.py
@@ -1,3 +1,4 @@
+import logging
 import io
 import queue
 from typing import Any
@@ -6,18 +7,27 @@ import pytest
 
 from picologging import INFO, Logger, StreamHandler, handlers
 
-logger = Logger("test", INFO)
+pico_logger = Logger("test", INFO)
 stream = io.StringIO()
 stream_handler = StreamHandler(stream)
 q = queue.Queue()
 listener = handlers.QueueListener(q, stream_handler)
 listener.start()
 handler = handlers.QueueHandler(q)
-logger.addHandler(handler)
+pico_logger.addHandler(handler)
 
 
 @pytest.mark.xfail(reason="https://github.com/microsoft/picologging/issues/90")
 def test_queue_listener_handler(caplog: Any) -> None:
     with caplog.at_level("INFO"):
-        logger.info("Testing now!")
+        pico_logger.info("Testing now!")
+        assert "Testing now!" in caplog.text
+
+
+stdlib_logger = logging.getLogger()
+
+# the test below is a demonstration that the above test is well-formed.
+def test_stdlib_logger(caplog: Any) -> None:
+    with caplog.at_level("INFO"):
+        stdlib_logger.info("Testing now!")
         assert "Testing now!" in caplog.text
