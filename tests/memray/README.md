2b5c6d34fe9362614e0b942693a24ce59dab16a0
Anthony Shaw <anthony.p.shaw@gmail.com>
2022-10-12 11:22:40 +1100

    Create a makefile for memray testing and a README see #112

---
+++ tests/memray/README.md
@@ -0,0 +1,13 @@
+# Memray tests
+
+Install memray (only macOS and Linux supported):
+
+```console
+$ pip install memray
+```
+
+Run memray on each file and start the HTTP server:
+
+```console
+$ make profiles
+```
---
+++ tests/memray/memray_logger.py
@@ -0,0 +1,26 @@
+"""
+Create 100,000  logger instances and run a log test on them
+"""
+
+import picologging
+from io import StringIO
+
+
+def log(level=picologging.INFO):
+    logger = picologging.Logger("test", level)
+    tmp = StringIO()
+
+    handler = picologging.StreamHandler(tmp)
+    handler.setLevel(level)
+    formatter = picologging.Formatter(
+        "%(created)f/%(asctime)s %(pathname)s:%(module)s:%(filename)s:%(lineno)d %(funcName)s %(levelno)d %(name)s - %(levelname)s %(process)d %(thread)d- %(message)s"
+    )
+    handler.setFormatter(formatter)
+    logger.handlers.append(handler)
+    
+    logger.debug("There has been a picologging issue")
+
+
+if __name__ == "__main__":
+    for _ in range(100_000):
+        log()
