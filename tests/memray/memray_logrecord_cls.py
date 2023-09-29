3842f28aeb2aef27a263c651904e4d36ec80deb8
Anthony Shaw <anthony.p.shaw@gmail.com>
2023-09-29 05:10:17 +0000

    black formatting

--- tests/memray/memray_logrecord_cls.py
+++ tests/memray/memray_logrecord_cls.py
@@ -1,17 +1,16 @@
-
 import picologging
 from picologging import LogRecord
 
 
 def log():
-
     for _ in range(100_000):
-        record = LogRecord("hello", picologging.WARNING, __file__, 123, "bork %s", None, None)
+        record = LogRecord(
+            "hello", picologging.WARNING, __file__, 123, "bork %s", None, None
+        )
         assert record.message is None
         assert record.getMessage() == "bork %s"
         assert record.message == "bork %s"
         assert "name" in record.__dict__
-
 
 
 if __name__ == "__main__":
