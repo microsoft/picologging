47316bc2f2a23130fdfe71c8d9260a47377e3b0d
Anthony Shaw <anthony.p.shaw@gmail.com>
2023-09-29 05:36:54 +0000

    Verify the integration of coloredlogs is fixed

---
+++ tests/integration/test_coloredlogs.py
@@ -0,0 +1,23 @@
+import picologging as logging
+import io
+import coloredlogs
+
+
+def test_coloredlogs_logger():
+    # Setup colored logger
+    stream = io.StringIO()
+
+    logger = logging.getLogger()
+    logger.addHandler(logging.StreamHandler(stream))
+    coloredlogs.install(logger=logger, level="INFO")
+
+    logger.info("Info message")
+    logger.warning("Warning message")
+    logger.error("Error message")
+    logger.critical("Critical message")
+
+    log = stream.getvalue()
+    assert "Info message" in log
+    assert "Warning message" in log
+    assert "Error message" in log
+    assert "Critical message" in log
