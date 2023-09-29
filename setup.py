540232b14b4ceb44e6fc48f6a2b02a60f6621b5e
Anthony Shaw <anthony.p.shaw@gmail.com>
2023-09-29 18:54:31 +1000

    Add explicit support for 3.12

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     install_requires=[],
     python_requires=">=3.7",
