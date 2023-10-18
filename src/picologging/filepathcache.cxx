6acce0c200179400693d057e66cf911e4680f533
Anthony Shaw <anthony.p.shaw@gmail.com>
2023-10-18 15:45:34 +1100

    Replace the custom filepath cache with a Python dictionary to see if its faster.

--- src/picologging/filepathcache.cxx
+++ src/picologging/filepathcache.cxx
@@ -3,36 +3,26 @@
 
 namespace fs = std::filesystem;
 
-const FilepathCacheEntry& FilepathCache::lookup(PyObject* pathname){
-    /*
-     * Notes: A vector ended up being significantly faster than an unordered_map,
-     * even though an unordered_map should probably be used.
-     * TODO #3 : Cap vector size or decide on a better map type.
-     */
-    Py_hash_t hash = PyObject_Hash(pathname);
-    for (auto& entry : cache){
-        if (entry.first == hash){
-            return entry.second;
-        }
+PyObject* lookup(PyObject* cache, PyObject* pathname){
+    PyObject* result = PyDict_GetItem(cache, pathname);
+    if (result != NULL){
+        return result; // todo : decide on refcnt.
     }
-    FilepathCacheEntry* entry = new FilepathCacheEntry();
+
     fs::path fs_path = fs::path(PyUnicode_AsUTF8(pathname));
+    PyObject *filename = nullptr, *module = nullptr;
 #ifdef WIN32
     const wchar_t* filename_wchar = fs_path.filename().c_str();
     const wchar_t* modulename = fs_path.stem().c_str();
-    entry->filename = PyUnicode_FromWideChar(filename_wchar, wcslen(filename_wchar)),
-    entry->module = PyUnicode_FromWideChar(modulename, wcslen(modulename));
+    filename = PyUnicode_FromWideChar(filename_wchar, wcslen(filename_wchar)),
+    module = PyUnicode_FromWideChar(modulename, wcslen(modulename));
 #else
-    entry->filename = PyUnicode_FromString(fs_path.filename().c_str());
-    entry->module = PyUnicode_FromString(fs_path.stem().c_str());
+    filename = PyUnicode_FromString(fs_path.filename().c_str());
+    module = PyUnicode_FromString(fs_path.stem().c_str());
 #endif
-    cache.push_back({hash, *entry});
-    return *entry;
-}
-
-FilepathCache::~FilepathCache(){
-    for (auto& entry : cache){
-        Py_CLEAR(entry.second.filename);
-        Py_CLEAR(entry.second.module);
-    }
+    PyObject *cacheItem = PyTuple_Pack(2, module, filename);
+    PyDict_SetItem(cache, pathname, cacheItem);
+    // TODO : I think this needs a decref to set the tuple total refcnt to 1 
+    // Otherwise it won't get GC'ed when this module is unloaded (which is when Python is shutdown so it doesn't matter much)
+    return cacheItem;
 }
